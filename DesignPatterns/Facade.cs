be304e50538849fa155b48e73b5928d45979f10a
clement lion <clement.lionceau@gmail.com>
2026-02-02 11:49:47 +0100

    added facade pattern

--- DesignPatterns/Facade.cs
+++ DesignPatterns/Facade.cs
@@ -1,6 +1,6 @@
 /*
 A structural pattern to hide operations behind a facade with which the user can interact.
-Make the subsystem easier to use.
+Make the subsystem easier to use, we want to abstract the difficulty.
 */
 
 public enum AttackType
@@ -14,49 +14,34 @@ public enum AttackType
 
 public class AttackFacade
 {
-    private int _baseDamage;
-    private AttackType _attackType;
+    private readonly RNGService _rng = new();
+    private readonly ElementalEngine _elemental = new();
 
-    public AttackFacade(int baseDamage, AttackType attackType)
+    public int ExecuteAttack(int baseDamage, AttackType type, ICharacter target)
     {
-        _baseDamage = baseDamage;
-        _attackType = attackType;
-    }
-
-    public int Attack(ICharacter target)
-    {
-        if (CheckIfMiss(target))
-        {
+        if (_rng.IsMiss())
             return 0;
-        }
 
-        int attackDamage = _baseDamage;
-        if (CheckIfTargetIsWeak(target))
-        {
-            attackDamage *= 2;
-        }
-        if (IsCriticalHit())
-        {
-            attackDamage *= 2;
-        }
+        int damage = baseDamage;
+        if (_elemental.IsWeak(target, type))
+            damage *= 2;
+        if (_rng.IsCritical())
+            damage *= 2;
 
-        return attackDamage;
-    }
-
-    public bool CheckIfMiss(ICharacter target)
-    {
-        return false;
-    }
-
-    public bool IsCriticalHit()
-    {
-        return true;
-    }
-
-    public bool CheckIfTargetIsWeak(ICharacter target)
-    {
-        return true;
+        return damage;
     }
 }
 
 public interface ICharacter { }
+
+public class RNGService
+{
+    public bool IsCritical() => true;
+
+    public bool IsMiss() => false;
+}
+
+public class ElementalEngine
+{
+    public bool IsWeak(ICharacter target, AttackType type) => true;
+}
