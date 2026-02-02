5b0a584bc757d5d9dbc05d6153e6100fd0707bc5
clement lion <clement.lionceau@gmail.com>
2026-02-02 11:07:04 +0100

    added coffee decorator

---
+++ DesignPatterns/Decorator.cs
@@ -0,0 +1,65 @@
+/*
+Wrap around a class in order to add additional functionalities.
+*/
+
+public interface ICoffee
+{
+    public bool Drink(int volumeToDrink);
+    public string GetSmell();
+}
+
+public class Coffee : ICoffee
+{
+    private int _volume;
+
+    public Coffee(int volume)
+    {
+        _volume = volume;
+    }
+
+    public bool Drink(int volumeToDrink)
+    {
+        if (_volume >= volumeToDrink)
+        {
+            _volume -= volumeToDrink;
+            return true;
+        }
+        return false;
+    }
+
+    public string GetSmell()
+    {
+        return "The fragrance of black coffee";
+    }
+}
+
+public abstract class CoffeeDecoratorBase : ICoffee
+{
+    private ICoffee _coffee;
+
+    public CoffeeDecoratorBase(Coffee coffee)
+    {
+        _coffee = coffee;
+    }
+
+    public bool Drink(int volumeToDrink)
+    {
+        return _coffee.Drink(volumeToDrink);
+    }
+
+    public virtual string GetSmell()
+    {
+        return _coffee.GetSmell();
+    }
+}
+
+public class CoffeeWithCinnaon : CoffeeDecoratorBase
+{
+    public CoffeeWithCinnaon(Coffee coffee)
+        : base(coffee) { }
+
+    public override string GetSmell()
+    {
+        return $"{base.GetSmell()} with a touch of cinnamon";
+    }
+}
