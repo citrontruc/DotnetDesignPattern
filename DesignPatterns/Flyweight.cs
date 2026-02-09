fd1b5ef28f6a9713b051e3765e99a8dbdc79c30c
clement lion <clement.lionceau@gmail.com>
2026-02-09 10:31:28 +0100

    added flyweight design pattern

---
+++ DesignPatterns/Flyweight.cs
@@ -0,0 +1,104 @@
+/*
+Flyweight: does not require object identity, for objects with a big number of elements that need to be mutualized because we have limited memory / process time.
+*/
+
+public enum EnemyTypes
+{
+    Bat,
+    Zombie,
+}
+
+public class Texture
+{
+    public int SizeX;
+    public int SizeY;
+}
+
+public interface ITextureObject
+{
+    public (int, int) GetSize();
+    public Texture? GetTexture();
+    protected void LoadTexture(string textureDirectory);
+    public void Draw(int x, int y);
+}
+
+public class TextureWrapper : ITextureObject
+{
+    private Texture? _texture;
+
+    public TextureWrapper() { }
+
+    public TextureWrapper(string textureDirectory)
+    {
+        LoadTexture(textureDirectory);
+    }
+
+    public TextureWrapper(Texture texture)
+    {
+        _texture = texture;
+    }
+
+    public void Draw(int x, int y)
+    {
+        Console.WriteLine($"Drawing texture in position {x}, {y}.");
+    }
+
+    public (int, int) GetSize()
+    {
+        if (_texture is null)
+        {
+            return (0, 0);
+        }
+        return (_texture.SizeX, _texture.SizeY);
+    }
+
+    public Texture? GetTexture()
+    {
+        return _texture;
+    }
+
+    public void LoadTexture(string textureDirectory)
+    {
+        Console.WriteLine($"Loading texture saved at {textureDirectory}.");
+    }
+}
+
+public class Ennemy
+{
+    public int PositionX;
+    public int PositionY;
+    public ITextureObject Texture;
+
+    public Ennemy(int x, int y, ITextureObject texture)
+    {
+        PositionX = x;
+        PositionY = y;
+        Texture = texture;
+    }
+
+    public void Draw()
+    {
+        Texture.Draw(PositionX, PositionY);
+    }
+}
+
+public class EnnemyFactory
+{
+    private Dictionary<EnemyTypes, string> _ennemyTextureDirectory;
+    private Dictionary<EnemyTypes, ITextureObject> _ennemyTypeTextures = new();
+
+    public EnnemyFactory(Dictionary<EnemyTypes, string> enemyTextureDirectory)
+    {
+        _ennemyTextureDirectory = enemyTextureDirectory;
+    }
+
+    public Ennemy CreateEnnemy(int positionX, int positionY, EnemyTypes enemyTypes)
+    {
+        if (!_ennemyTypeTextures.ContainsKey(enemyTypes))
+        {
+            TextureWrapper textureWrapper = new(_ennemyTextureDirectory[enemyTypes]);
+            _ennemyTypeTextures[enemyTypes] = textureWrapper;
+        }
+        return new Ennemy(positionX, positionY, _ennemyTypeTextures[enemyTypes]);
+    }
+}
